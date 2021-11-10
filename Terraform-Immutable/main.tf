2fb9124a9fbcbee399799c526d429a12790f67a4
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-10 14:04:44 +0000

    adding new file

--- Terraform-Immutable/main.tf
+++ Terraform-Immutable/main.tf
@@ -1,5 +1,5 @@
 module "cart" {
-    source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/immutable"
+    source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/immutable//APP-module"
     env = var.env
     instance_type = var.instance_type
     component   = "cart"
